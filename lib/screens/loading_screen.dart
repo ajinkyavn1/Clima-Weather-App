1d20d4c6c752cdd998e65812c68a45fc900fe375
ajinkyavn1 <ajinkyanarkhede7@gmail.com>
2021-06-25 22:25:09 +0530

    Call Api

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
@@ -11,9 +14,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   void initState() {
     getLocation();
+    getData();
     super.initState();
   }
+  void getData()async{
 
+    Response response=await get("https://api.openweathermap.org/data/2.5/find?lat=20.9&lon=76.2&cnt=10&appid=b8182205d4316687448631dcda94330a") as Response;
+    var Json=JsonDecoder()
+
+  }
+  
 void getLocation() async{
   Location location=Location();
     await location.getLocation();
@@ -34,4 +44,6 @@ void getLocation() async{
       ),
     );
   }
+  
+
 }
